a937c54de7ae0f89fc8936c0296eb79b8384a87c
Rahul Patil <rahulpatil281292@gmail.com>
2019-12-24 00:07:59 +0530

    Updated project folder structure.

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Macbook on 24/12/19.
+//  Copyright © 2019 com.rahulpatil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
